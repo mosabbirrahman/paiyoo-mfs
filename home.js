6367af2ed47778e3b20b32407b15d9fe038c20ee
mosabbirrahman <mdmosabbirrahman07@gmail.com>
2025-01-19 17:18:07 +0600

    pinNumber

--- home.js
+++ home.js
@@ -1,7 +1,42 @@
 document.getElementById('add-money').addEventListener('click', function(e){
- e.preventDefault();
- const amountNumber = document.getElementById('amount-number').value;
- console.log(amountNumber);
- const pinNumber = document.getElementById('pin-number').value;
- console.log(pinNumber);
+    e.preventDefault();
+    const amountNumber = document.getElementById('amount-number').value;
+    console.log(amountNumber);
+    const pinNumber = document.getElementById('pin-Number').value;
+    console.log(pinNumber);
+
+if(pinNumber === '123'){
+
+    const balance = document.getElementById('main-balance').innerText;
+    console.log(balance)
+
+    const addMoney = parseFloat(amountNumber);
+    const mainBalance = parseFloat(balance);
+    const newBalance = addMoney + mainBalance;
+    console.log(newBalance);
+    document.getElementById('main-balance').innerText = newBalance;
+}
+else{
+    alert('faild pass')
+}
+    
 })
+
+
+
+
+
+const handlecashInOut = (e)=>{
+    const balanceStr = document.getElementById('main-balance');
+    const balance = parseFloat(balanceStr.innerText);
+
+    if(e){
+        const newBalance = balance + 100;
+        balanceStr.innerText = newBalance;
+    }else{
+        const newBalance = balance - 100;
+        balanceStr.innerText = newBalance;
+    }
+
+    
+} 
