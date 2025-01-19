8275cb870feebd7c59de1ead874c9f1e324e7f16
mosabbirrahman <mdmosabbirrahman07@gmail.com>
2025-01-19 07:11:10 +0600

    home page

--- login.js
+++ login.js
@@ -1,9 +1,17 @@
 document.getElementById('btn-login').addEventListener('click',function(e){
   e.preventDefault();
-  console.log('btn clciked')
+  
 
   const PhoneNumber = document.getElementById('phone-number').value;
   console.log(PhoneNumber);
   const pinNumber = document.getElementById('pin-number').value;
   console.log(pinNumber)
+
+  if(PhoneNumber === '5' && pinNumber === '123'){
+    console.log('you are loged in')
+    window.location.href = '/home.html';
+  }
+  else{
+    alert('worng phone number')
+  }
 })
